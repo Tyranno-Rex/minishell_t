dea519146a588a476acfd45c4cee35b31bcbad2a
zinnnn37 <zinnnn37@gmail.com>
2023-03-18 18:54:15 +0900

    [quotes.c] done splite by quotes

--- minjinki/src/PARSE/quotes.c
+++ minjinki/src/PARSE/quotes.c
@@ -6,20 +6,20 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 16:24:44 by minjinki          #+#    #+#             */
-/*   Updated: 2023/03/18 14:31:16 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/03/18 18:51:32 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-t_token	*malloc_node(int len, char *start)
+t_token	*malloc_node(int len)
 {
 	t_token	*new;
 
 	new = ft_calloc(1, sizeof(t_token));
 	if (!new)
 	{
-		ft_lstclear(&(g_glob.tok));	//add del func
+		ft_lstclear(&(g_glob.tok));
 		print_error("Fail to allocate memory: g_glob.tok\n");
 		return (NULL);
 	}
@@ -27,30 +27,60 @@ t_token	*malloc_node(int len, char *start)
 	if (!(new->data))
 	{
 		free(new);
-		ft_lstclear(&(g_glob.tok));	//add del func
+		ft_lstclear(&(g_glob.tok));
 		print_error("Fail to allocate memory: g_glob.tok->data\n");
 		return (NULL);
 	}
-	ft_strlcpy(new->data, start, len + 1);
-	printf("%s\n", new->data);	/*** remove ***/
 	return (new);
 }
 
-int	split_quote(int len, char *start, char *cmd)
+t_token	*add_node(int type, int len, char *start)
 {
-	int end;	// save index of 2nd quote
+	t_token	*new;
 
-	ft_lstadd_back(&(g_glob.tok), malloc_node(len, start));
-	if (*cmd == '\'')
-		end = single_quote();	// check till single quote appears
-	else if (*cmd == '\"')
-		end = double_quote();	// check till double quote appears
-	return (end + 1);	// return next index of closed quote
+	new = malloc_node(len);
+	new->type = type;
+	ft_strlcpy(new->data, start, len + 1);
+	printf("type: %d\n", new->type);
+	printf("data: %s\n\n", new->data); /*** remove ***/
+	return (new);
+}
+
+int	quotes(int type, char *cmd)
+{
+	int	len;
+
+	len = ft_strnlen(cmd + 1, type);
+	if (len == -1)
+	{
+		ft_lstclear(&(g_glob.tok));
+		print_error("syntax error: unclosed quotes\n");
+		return (ERROR);
+	}
+	ft_lstadd_back(&(g_glob.tok), add_node(type, len, cmd + 1));
+	return (len);
+}
+
+int	split_quote(int len, char *cmd, char *quote)
+{
+	int	end;	// save index of 2nd quote
+
+	end = 0;
+	if (cmd != quote)
+		ft_lstadd_back(&(g_glob.tok), add_node(STR, len, cmd));
+	if (*quote == SINGLE)
+		end = quotes(SINGLE, quote);	// check till single quote appears
+	else if (*quote == DOUBLE)
+		end = quotes(DOUBLE, quote);	// check till double quote appears
+	if (end == ERROR)
+		return (ERROR);
+	return (end);	// return index of closed quote
 }
 
 void	deal_quotes(char *cmd)
 {
 	int		i;
+	int		gap;
 	int		last;
 	char	*start;
 
@@ -59,13 +89,18 @@ void	deal_quotes(char *cmd)
 	start = cmd;
 	while (cmd[++i])
 	{
-		if (cmd[i] == '\'' || cmd[i] == '\"')
+		if (cmd[i] == SINGLE || cmd[i] == DOUBLE)
 		{
-			i = split_quote(i - last, start, cmd + i);
-			start = cmd + i;
-			last = i;
+			gap = split_quote(i - last, start, cmd + i);
+			if (gap == ERROR)
+				return ;
+			i += (gap + 1);
+			start = cmd + i + 1;
+			last = i + 1;
 		}
 	}
+	if (*start)
+		split_quote(i + 1, start, cmd);
 }
 /*
 ** deal_quot() i랑 split_quote() len이랑 꼬임 >> cmd 스캔 변수랑 split하는 길이 변수 다시 구분하고 확인
