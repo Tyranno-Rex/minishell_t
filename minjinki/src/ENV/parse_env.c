696e3ed3f396ded62f7f5eb0367c820c1806e972
zinnnn37 <zinnnn37@gmail.com>
2023-03-20 17:59:10 +0900

    [ENV] add comments

--- minjinki/src/ENV/parse_env.c
+++ minjinki/src/ENV/parse_env.c
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 16:57:08 by minjinki          #+#    #+#             */
-/*   Updated: 2023/03/20 17:04:05 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/03/20 17:23:15 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 t_bool	parse_env(void)
 {
+	// make linked list
+	// add OLDPWD -> default as HOME dir
 	return (TRUE);
 }
