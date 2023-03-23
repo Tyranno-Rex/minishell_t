300e00b8cbce62781b2868d1c1ad390f0f8aef3d
zinnnn37 <zinnnn37@gmail.com>
2023-03-23 17:51:27 +0900

    [LIBFT] add ft_strcmp

--- minjinki/src/LIBFT/libft.h
+++ minjinki/src/LIBFT/libft.h
@@ -6,7 +6,7 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 15:25:24 by minjinki          #+#    #+#             */
-/*   Updated: 2023/03/18 19:41:02 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/03/23 17:50:50 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ int		ft_is_space(char c);
 int		ft_lstsize(t_token *lst);
 int		ft_matrixlen(char **matrix);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
