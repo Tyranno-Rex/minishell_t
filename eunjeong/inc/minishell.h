fd504ae52df9a96c6ca2052801c1014586041f8d
정은성 <80687043+CoderTrex@users.noreply.github.com>
2023-04-14 00:14:43 +0900

    파일 디스크립터 수정

--- eunjeong/inc/minishell.h
+++ eunjeong/inc/minishell.h
@@ -2,16 +2,48 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <stdbool.h>
+
 #include "../libft/libft.h"
+
+
+
 
 
 typedef struct s_export
 {
-    char        *name;
-    char        *data;
-    t_export    *next;
+    char        	*name;
+    char        	*data;
+    struct s_export	*next;
 } t_export;
+
+
+typedef struct s_data
+{
+	char	**full_cmd;
+	char	*full_path;
+	int		infile;
+	int		outfile;
+}			t_data;
+
+
+
+
+/*pipe.c*/
+static t_data   *data_init(void);
+static t_data   *set_cmd_content(t_data *node, char **argv[2], int *i);
+int 			pipex(char **argv);
+static char		**get_trimmed(char **args);
+
+/*utils.c*/
+static t_list	*stop_fill(t_list *cmds, char **args, char **contents);
+static int		ft_count_len(char const *s1);
+char			*ft_strtrim_all(char const *s1, int check_S, int check_L);
+void			ft_free_matrix(char ***m);
+int				ft_matrixlen(char **m);
+char			**ft_dup_matrix(char **m);
+
 
 
 
---
+++ eunjeong/pipe/fd_return.c
@@ -0,0 +1,60 @@
+#include "../inc/minishell.h"
+
+// 파일 디스크립터를 반환해서 해당 파일에서 작업을 진행
+int get_fd(int oldfd, char *path, int flags[2])
+{
+	int fd;	// 가져온 파일 디스크립터를 저장할 변수
+	// 이전 파일 디스크립터가 2보다 크다면, 닫는다.
+	if (oldfd > 2)
+		close(oldfd);
+	// 파일 경로가 없다면 -1을 반환한다.
+	if (!path)
+		return (-1);
+	// 파일에 대한 접근 권한이 X 또는 열 수 없음
+	if (access(path, F_OK) == -1 && !flags[0])
+		mini_perror(NDIR, path, 127);
+	else if (!flags[0] && access(path, R_OK) == -1)
+		mini_perror(NPERM, path, 126);
+	else if (flags[0] && access(path, W_OK) == -1 && access(path, F_OK) == 0)
+		mini_perror(NPERM, path, 126);
+	// 파일 플래그를 확인하고 open 함수를 통해 파일 디스크립터 제공.
+	if (flags[0] && flags[1])
+		fd = open(path, O_CREAT | O_WRONLY | O_APPEND, 0666);
+	else if (flags[0] && !flags[1])
+		fd = open(path, O_CREAT | O_WRONLY | O_TRUNC, 0666);
+	else if (!flags[0] && oldfd != -1)
+		fd = open(path, O_RDONLY);
+	else
+		fd = oldfd;
+	// 가져온 파일 디스크립터를 반환한다.
+	return (fd);
+}
+
+// 입출력 파일 가져오는 기본 예시 함수
+t_mini	*get_file(t_mini *node, char **args, int *i)
+{
+	char	*error_msg;	// 적절한 에러 메시지를 저장할 변수
+	int		flags[2];	// 파일 플래그를 저장할 배열
+
+	// 파일 플래그 초기화
+	flags[0] = 1;
+	flags[1] = 0;
+	// 에러 메시지 초기화
+	error_msg = "error! fd number get wrong value! error!'";
+	(*i)++;	// i를 1 증가시킨다.
+	if (args[*i])
+		// 가져온 출력 파일 디스크립터를 node->outfile에 저장한다.
+		node->outfile = get_fd(node->outfile, args[*i], flags);
+	if (!args[*i] || node->outfile == -1)	// 가져온 디스크립터가 유효하지 않은 경우
+	{
+		*i = -1;	// i를 -1로 초기화한다.
+		if (node->outfile != -1)	// 이미 파일 디스크립터를 가져온 경우
+		{
+			ft_putendl_fd(error_msg, 2);	// 적절한 에러 메시지
+			g_status = 2;
+		}
+		else
+			g_status = 1;
+	}
+	return (node);
+}
