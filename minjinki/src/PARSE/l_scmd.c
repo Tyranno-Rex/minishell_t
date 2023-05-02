178bff182fd9d5c8082ad7a6b2ecd33e27e18141
zinnnn37 <zinnnn37@gmail.com>
2023-05-02 17:15:53 +0900

    [PARSE] scmd: til saving options of command

--- minjinki/src/PARSE/l_scmd.c
+++ minjinki/src/PARSE/l_scmd.c
@@ -6,11 +6,23 @@
 /*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 13:58:01 by minjinki          #+#    #+#             */
-/*   Updated: 2023/05/02 16:45:55 by minjinki         ###   ########.fr       */
+/*   Updated: 2023/05/02 17:15:29 by minjinki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
+
+char	*join_ori(char *s1, char *s2)
+{
+	char	*res;
+
+	res = ft_strjoin(s1, s2);
+	if (!res)
+		return (NULL);
+	free(s1);
+	s1 = NULL;
+	return (res);
+}
 
 t_scmd	add_scmd(t_token *token)
 {
@@ -22,11 +34,18 @@ t_scmd	add_scmd(t_token *token)
 	if (token)
 	{
 		scmd = ft_calloc(1, sizeof(t_scmd));
-		// scmd에 CMD만 (strdup 사용) -> 현재 노드만
-		// opt에 CMD 다음 OPT 넣기.. (-로 시작하는 것들)
-		// ori에 cmd 전체(e.g. echo -n a b c" d") -> strjoin
+		scmd->scmd = ft_strdup(token->data);	// simple cmd
+		scmd->ori = ft_strdup(token->data);
+		if (!(scmd->scmd) || !(scmd->ori))
+			return (NULL);
+		token = token->next;
+		scmd->opt = ft_strdup(token->data);		// options
+		scmd->ori = join_ori(scmd->ori, token->data);
+		if (!(scmd->opt) || !(scmd->ori))
+			return (NULL);
 		//	-> 중복은 지우고 중복 아닌 건 join으로 합쳐서 -abc... 형태로
 		// av에는 OPT 이하 ARGV들 저장
+		// ori에 cmd 전체(e.g. echo -n a b c" d") -> strjoin
 	}
 	return (scmd);
 }
