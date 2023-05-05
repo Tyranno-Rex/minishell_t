8ae4aec1eca223e07dcf1e2466f6fb89170fee53
Eun Seong Jeong <eunjeong@c9r1s8.42seoul.kr>
2023-05-05 16:39:25 +0900

    누나 파이팅

--- eunjeong/src/BUILTIN/unset.c
+++ eunjeong/src/BUILTIN/unset.c
@@ -1,57 +1,57 @@
 #include "../../include/minishell.h"
 
-void	env_select_clear(char *env_value)
-{
-	char *new_env_key;
+// void	env_select_clear(char *env_value)
+// {
+// 	char *new_env_key;
 
-	while (g_glob.env)
-	{
-		if (g_glob.env->key == env_value)
-		{
-			if (g_glob.env->next)
-			{
-				new_env_key = g_glob.env->next->key;
+// 	while (g_glob.env)
+// 	{
+// 		if (g_glob.env->key == env_value)
+// 		{
+// 			if (g_glob.env->next)
+// 			{
+// 				new_env_key = g_glob.env->next->key;
 				
-                free(g_glob.env);
-				g_glob = g_glob;
+//                 free(g_glob.env);
+// 				g_glob = g_glob;
 
-			}
-			else
-				free(g_glob.env);
-		}
-		g_glob.env = g_glob.env->next;
-	}
-}
+// 			}
+// 			else
+// 				free(g_glob.env);
+// 		}
+// 		g_glob.env = g_glob.env->next;
+// 	}
+// }
 
-void check_list_del(char *env_list)
-{
-	t_glob		tmp;
+// void check_list_del(char *env_list)
+// {
+// 	t_glob		tmp;
 
-	tmp = g_glob;
-	while (tmp.env)
-	{
-		if (!strncmp(tmp.env->key, env_list, ft_strlen(env_list)))
-		{
-			env_select_clear(env_list);
-			break;
-		}
-		tmp.env = tmp.env->next;
-	}	
-}
+// 	tmp = g_glob;
+// 	while (tmp.env)
+// 	{
+// 		if (!strncmp(tmp.env->key, env_list, ft_strlen(env_list)))
+// 		{
+// 			env_select_clear(env_list);
+// 			break;
+// 		}
+// 		tmp.env = tmp.env->next;
+// 	}	
+// }
 
-void    ft_unset(void)
-{
-    t_glob  tmp;
-    int     count = 0;
+// void    ft_unset(void)
+// {
+//     t_glob  tmp;
+//     int     count = 0;
 
 		
-    tmp = g_glob;
-    while (tmp.tok)
-    {
-		if (tmp.tok->type == 1 && count < 1)
-			count++;
-		else if (tmp.tok->type == 1 && count >= 1)
-			check_list_del(tmp.tok->data);
-        tmp.tok = tmp.tok->next;
-    }
-}
+//     tmp = g_glob;
+//     while (tmp.tok)
+//     {
+// 		if (tmp.tok->type == 1 && count < 1)
+// 			count++;
+// 		else if (tmp.tok->type == 1 && count >= 1)
+// 			check_list_del(tmp.tok->data);
+//         tmp.tok = tmp.tok->next;
+//     }
+// }
