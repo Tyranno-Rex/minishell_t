b6d29f5ea4a66699fbbb586eb9b34699f1ad92ce
정은성 <80687043+CoderTrex@users.noreply.github.com>
2023-05-11 11:44:32 +0900

    comment 추가

--- eunjeong/src/MAIN/single_builtin.c
+++ eunjeong/src/MAIN/single_builtin.c
@@ -136,19 +136,19 @@ int	open_redirect(t_token *redir)
 	int		fd;
 	char	*err_msg;
 
-	err_msg = ft_strjoin("minishell: ", redir->data);
+	err_msg = ft_strjoin("minishell: ", redir->next->data);
 	// 문제는 filename에 대한 확신이 없음
-	// filename이 옆에 꺼라고 > 생각하니까 next의 next라고 정의함.
-	if (!ft_strcmp(redir->next->data, ">") || !ft_strcmp(redir->next->data, ">>"))
+	// filename이 옆에 꺼라고 > 생각하니까 next의 data라고 정의함.
+	if (!ft_strcmp(redir->data, ">") || !ft_strcmp(redir->data, ">>"))
 	{
-		// 출력 리다이렉션(새 파일 생성)일 경우
+		// 출력 리다이렉션일 경우 (덮어 쓰기)
 		if (!ft_strcmp(redir->data, ">>") )
 			// fd = open(redir->filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
-			fd = open(redir->next->next->data, O_WRONLY | O_CREAT | O_APPEND, 0644);
-		// 출력 리다이렉션일 경우 (덮어 쓰기)
+			fd = open(redir->next->data, O_WRONLY | O_CREAT | O_APPEND, 0644);
+		// 출력 리다이렉션(새 파일 생성)일 경우
 		else
 			// fd = open(redir->filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			fd = open(redir->next->next->data, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			fd = open(redir->next->data, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		if (fd == -1)
 			return (perror(err_msg), free(err_msg), -1);
 		(ft_dup2(fd, STDOUT_FILENO), close(fd));
@@ -162,10 +162,10 @@ int	open_redirect(t_token *redir)
 		// 입력 리다이렉션(Here Document)일 경우
 		if (!ft_strcmp(redir->data, "<<"))
 			// ft_unlink(redir->filename);
+			// file name이 맞다면, 아래 함수는 동일함
 			ft_unlink(redir->next->data);
 		(ft_dup2(fd, STDIN_FILENO), close(fd));
 	}
-	printf("hello\n");
 	free(err_msg);
 	return (fd);
 }
@@ -209,17 +209,32 @@ int	check_single_builtin()
 	int		origin_io[2];
 	int		builtin_num;
 
-	proc_data = g_glob.tok;
+
 	builtin_num = 0;
+
+	// bt_fp = NULL;
+	// 해당 문제는 함수 포인터임 -> 함수 포인터 대신에 함수를 호출하는 방식으로 바꿈 
+
+	// cmd_argv = cmd_list2arr(&proc_data->cmd_list);
 	cmd_argv = make_tok2D();
+	
+	
+	// 해당 문제는 전역 변수로 선언이 가능하기 때문에 아래로 대체
+	proc_data = g_glob.tok; 
+	// proc_data = list_peek_first_content(&data->proc_data_list);
+	
+	
 	if (cmd_argv[0])
 		// 실행해야하는 함수 찾기
 		builtin_num = is_builtin_num(cmd_argv[0]);
+	
+	// 빌트인 함수의 갯수를 세는 것이 필요함
 	// if (builtin_num && ft_matrixlenB(cmd_argv))
 	if (builtin_num)
 	{
-		printf("char: %s, number: %d\n", cmd_argv[0], builtin_num);
+		// 이건 똑같음
 		save_origin_io(origin_io);
+		
 		if (do_redirect(proc_data))
 			g_glob.exit_stat = 1;
 		else
