b62f1f192058accda4e8dba8c8f4e5b3123b0b28
Eun Seong Jeong <eunjeong@c6r10s7.42seoul.kr>
2023-05-11 19:40:33 +0900

    수정

--- eunjeong/src/MAIN/main.c
+++ eunjeong/src/MAIN/main.c
@@ -205,39 +205,6 @@ void	make_child()
 }
 
 
-// 시그널 처리 함수들
-
-static int	_wstatus(int status)
-{
-	int	return_val;
-
-	return_val = status & 0177;
-	return (return_val);
-}
-
-int	wifsignaled(int status)
-{
-	int	return_val;
-
-	return_val = ((_wstatus(status) != 0177) && (_wstatus(status) != 0));
-	return (return_val);
-}
-
-int	wtermsig(int status)
-{
-	int	return_val;
-
-	return_val = _wstatus(status);
-	return (return_val);
-}
-
-int	wexitstatus(int status)
-{
-	int	return_val;
-
-	return_val = ((unsigned)status >> 8) & 0xff;
-	return (return_val);
-}
 
 // 이거 뭐를 넘겨줘야 할지 모르겠음
 pid_t	ft_waitpid(pid_t pid, int *stat_loc, int options)
