b5c514ec26be00c50530b06db91adae1e9da5d63
zinnnn37 <zinnnn37@gmail.com>
2023-05-13 10:55:35 +0900

    [NORM] SIGNAL

--- project/eunjeong/src/SIGNAL/signal.c
+++ project/eunjeong/src/SIGNAL/signal.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signal.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minjinki <minjinki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/13 10:52:18 by minjinki          #+#    #+#             */
+/*   Updated: 2023/05/13 10:55:24 by minjinki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/minishell.h"
 
 void	init_rl_catch_signals(void)
@@ -7,20 +19,20 @@ void	init_rl_catch_signals(void)
 	rl_catch_signals = 0;
 }
 
-void sig_handler(int signal)
+void	sig_handler(int signal)
 {
-    if (signal == SIGINT)
-    {
+	if (signal == SIGINT)
+	{
 		printf("\n");
-        printf("\033[K"); //지워버리는 것을 원하지는 않았음.
-    }
-    if (rl_on_new_line() == -1) // readline으로 설정한 문자열을 한 번 출력한다?
-        exit(1);
-    rl_replace_line("", 1); // 프롬프트에 이미 친 문자열을 싹 날려준다.
-    rl_redisplay();         // 프롬프트 커서가 움직이지 않게 해준다.
+		printf("\033[K");
+	}
+	if (rl_on_new_line() == -1)
+		exit(1);
+	rl_replace_line("", 1);
+	rl_redisplay();
 }
 
-void setting_signal()
+void	setting_signal(void)
 {
 	signal(SIGINT, sig_handler);
 	signal(SIGQUIT, SIG_IGN);
